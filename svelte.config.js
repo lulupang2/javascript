9238e529359c0232a6e1b00ac37bc41924206002
lulupang2 <lulupang@live.co.kr>
2022-02-04 21:04:02 +0900

    배포

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
-
+import netlify from "@sveltejs/adapter-netlify";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -12,10 +11,10 @@ const config = {
   ],
 
   kit: {
-                adapter: netlify(), //<--- 추가
-                // hydrate the <div id="svelte"> element in src/app.html
-                target: '#svelte'
-        }
+    adapter: netlify(), //<--- 추가
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+  },
 };
 
 export default config;
